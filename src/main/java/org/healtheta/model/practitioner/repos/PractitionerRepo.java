4e139dbcae3a2ea75a0a0bfc3a8e15ede4cb3549
pjeyarat <pragash.jeyaratnam@gmail.com>
2018-10-24 18:47:50 +0530

    modified model

--- src/main/java/org/healtheta/model/practitioner/repos/PractitionerRepo.java
+++ src/main/java/org/healtheta/model/practitioner/repos/PractitionerRepo.java
@@ -1,5 +1,6 @@
 package org.healtheta.model.practitioner.repos;
 
+import org.healtheta.model.common.HumanName;
 import org.healtheta.model.practitioner.Practitioner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import java.util.List;
 @Repository
 public interface PractitionerRepo extends JpaRepository<Practitioner, Long> {
     public Practitioner findPractitionerById(Long id);
+    public List<Practitioner> findByNameIn(List<HumanName> humanNameList);
     public List<Practitioner> findAll();
 }
